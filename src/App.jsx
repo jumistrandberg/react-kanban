4e0b18198eb9d7a03175704176ae592eabe6bb60
Jumi <feliciastrandberg@gmail.com>
2024-04-02 14:42:33 +0200

    added sections for kanban containers. removed vite index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      <main>
+        <section className="todo-section"></section>
+        <section className="doing-section"></section>
+        <section className="done-section"></section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
