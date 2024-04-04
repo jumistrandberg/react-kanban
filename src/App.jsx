51b4faad363837a0ea9f77baf6a56c02999c5629
Jumi <feliciastrandberg@gmail.com>
2024-04-04 10:10:38 +0200

    removed content, starting over

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
 import "./App.css";
-const Card = () => {
-  
-}
 
-const Column = () => {
 
-}
 
-const Board = () => {
-  return (
-    <div className="board">
-      
-    </div>
-  )
-}
 function App() {
   return (
     <>
-    {/* <header>
-      <h2>Kanban</h2>
-    </header>
-      <main>
-        <section className="todo-section">
-          <h3>Todo</h3>
-          <button>Add task</button>
-        </section>
-        <section className="doing-section">
-          <h3>Doing</h3>
-        </section>
-        <section className="done-section">
-          <h3>Done</h3>
-        </section>
-      </main> */}
+ 
     </>
   );
 }
