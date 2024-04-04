2021dea0d2b81588dd8cf07ff93465a079572208
Jumi <feliciastrandberg@gmail.com>
2024-04-04 12:14:10 +0200

    created Card component with Draggable provided rendered prop

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Draggable } from "react-beautiful-dnd";
 
-const Card = () => {
+// Define Card component with props
+const Card = ({ title, index }) => {
   return (
-    <div>Card</div>
-  )
-}
+    <Draggable draggableId={title} index={index}>
+      {/* Render prop by Draggable  */}
+      {(provided) => {
+        <div
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+        >
+          {title}
+        </div>;
+      }}
+    </Draggable>
+  );
+};
 
-export default Card
+export default Card;
