fdd270ce6d91c05798e464cb6ea9b20047fd1645
Jumi <feliciastrandberg@gmail.com>
2024-04-07 23:25:26 +0200

    commented out everything to try again got confused

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,37 +1,62 @@
-import React from "react";
-import { IoMdTrash } from "react-icons/io";
-import { Droppable } from "react-beautiful-dnd";
+// import React, { useEffect, useState } from "react";
+// import { IoMdTrash } from "react-icons/io";
+// import { Droppable } from "react-beautiful-dnd";
+// import { useSearchParams } from "react-router-dom";
 
-const Column = ({ column, handleDeleteColumn }) => {
-  return (
-    <>
-      <div className="column">
-        <div className="col-top-part">
-          <div>{column.title}</div>
-          <button
-            aria-label="trash icon"
-            onClick={() => {
-              handleDeleteColumn(column.id);
-            }}
-          >
-            <IoMdTrash />
-          </button>
-        </div>
-        <Droppable droppableId={column.id.toString()}>
-        {(provided, snapshot) => (
-          <div
-            className="content-container"
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            Content
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-      </div>
-    </>
-  );
-};
+// const Column = ({ column, handleDeleteColumn }) => {
+//   const [editColName, setEditColName] = useState(false);
 
-export default Column;
+//   return (
+//     <>
+//       <div className="column">
+//         <div
+//           className="col-top-part"
+//           onClick={() => {
+//             setEditColName(true);
+//           }}
+//         >
+//           <div>
+//             {!editColName && column.title}{" "}
+//             {editColName && (
+//               <input
+//                 value={column.title}
+//                 onChange={(e) => updateColumn(column.id, e.target.value)}
+//                 onBlur={() => {
+//                   setEditColName(false);
+//                 }}
+//                 onKeyDown={(e) => {
+//                   if (e.key !== "Enter") return;
+//                   setEditColName(false);
+//                 }}
+//               />
+//             )}
+//           </div>
+
+//           <button
+//             aria-label="trash icon"
+//             onClick={() => {
+//               handleDeleteColumn(column.id);
+//             }}
+//           >
+//             <IoMdTrash />
+//           </button>
+//         </div>
+//         <Droppable droppableId={column.id.toString()}>
+//           {(provided, snapshot) => (
+//             <div
+//               className="content-container"
+//               ref={provided.innerRef}
+//               {...provided.droppableProps}
+//               isDraggingOver={snapshot.isDraggingOver}
+//             >
+//               Content
+//               {provided.placeholder}
+//             </div>
+//           )}
+//         </Droppable>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Column;
