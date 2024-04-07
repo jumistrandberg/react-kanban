c2336cb564f01e3c0652b0b063a99391728d2409
Jumi <feliciastrandberg@gmail.com>
2024-04-07 23:34:05 +0200

    added button to add new task

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,62 +1,63 @@
-// import React, { useEffect, useState } from "react";
-// import { IoMdTrash } from "react-icons/io";
-// import { Droppable } from "react-beautiful-dnd";
-// import { useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { IoMdTrash } from "react-icons/io";
+import { Droppable } from "react-beautiful-dnd";
+import { useSearchParams } from "react-router-dom";
 
-// const Column = ({ column, handleDeleteColumn }) => {
-//   const [editColName, setEditColName] = useState(false);
+const Column = ({ column, handleDeleteColumn }) => {
+  const [editColName, setEditColName] = useState(false);
 
-//   return (
-//     <>
-//       <div className="column">
-//         <div
-//           className="col-top-part"
-//           onClick={() => {
-//             setEditColName(true);
-//           }}
-//         >
-//           <div>
-//             {!editColName && column.title}{" "}
-//             {editColName && (
-//               <input
-//                 value={column.title}
-//                 onChange={(e) => updateColumn(column.id, e.target.value)}
-//                 onBlur={() => {
-//                   setEditColName(false);
-//                 }}
-//                 onKeyDown={(e) => {
-//                   if (e.key !== "Enter") return;
-//                   setEditColName(false);
-//                 }}
-//               />
-//             )}
-//           </div>
+  return (
+    <>
+      <div className="column">
+        <div
+          className="col-top-part"
+          onClick={() => {
+            setEditColName(true);
+          }}
+        >
+          <div>
+            {!editColName && column.title}{" "}
+            {editColName && (
+              <input
+                value={column.title}
+                onChange={(e) => updateColumn(column.id, e.target.value)}
+                onBlur={() => {
+                  setEditColName(false);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key !== "Enter") return;
+                  setEditColName(false);
+                }}
+              />
+            )}
+          </div>
 
-//           <button
-//             aria-label="trash icon"
-//             onClick={() => {
-//               handleDeleteColumn(column.id);
-//             }}
-//           >
-//             <IoMdTrash />
-//           </button>
-//         </div>
-//         <Droppable droppableId={column.id.toString()}>
-//           {(provided, snapshot) => (
-//             <div
-//               className="content-container"
-//               ref={provided.innerRef}
-//               {...provided.droppableProps}
-//               isDraggingOver={snapshot.isDraggingOver}
-//             >
-//               Content
-//               {provided.placeholder}
-//             </div>
-//           )}
-//         </Droppable>
-//       </div>
-//     </>
-//   );
-// };
+          <button
+            aria-label="trash icon"
+            onClick={() => {
+              handleDeleteColumn(column.id);
+            }}
+          >
+            <IoMdTrash />
+          </button>
+        </div>
+        <Droppable droppableId={column.id.toString()}>
+          {(provided, snapshot) => (
+            <div
+              className="content-container"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              isDraggingOver={snapshot.isDraggingOver}
+            >
+              Content
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+        <button onClick={() => handleCreateCard(column.id)}>New task</button>
+      </div>
+    </>
+  );
+};
 
-// export default Column;
+export default Column;
