522ee76e6b3204a3c34f39a33f12fa6a5941cb0b
Jumi <feliciastrandberg@gmail.com>
2024-04-07 23:35:45 +0200

    brought back commented out code

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,56 +1,56 @@
-// import React, { useState } from "react";
-// import { IoIosAddCircle } from "react-icons/io";
-// import Column from "./Column";
-// import { DragDropContext } from "react-beautiful-dnd";
+import React, { useState } from "react";
+import { IoIosAddCircle } from "react-icons/io";
+import Column from "./Column";
+import { DragDropContext } from "react-beautiful-dnd";
 
-// const Board = () => {
-//   const [columns, setColumns] = useState([]);
-//   console.log(columns);
+const Board = () => {
+  const [columns, setColumns] = useState([]);
+  console.log(columns);
 
-//   return (
-//     <>
-//       <DragDropContext>
-//         <div className="board">
-//           <div className="columns-output-container">
-//             {columns.map((col, index) => (
-//               <Column
-//                 key={index}
-//                 column={col}
-//                 handleDeleteColumn={handleDeleteColumn}
-//               />
-//             ))}
-//           </div>
-//           <button
-//             className="new-col-btn"
-//             onClick={() => {
-//               handleCreateColumn();
-//             }}
-//             aria-label="Add new column"
-//           >
-//             <span>New column</span>
-//             <IoIosAddCircle />
-//           </button>
-//         </div>
-//       </DragDropContext>
-//     </>
-//   );
+  return (
+    <>
+      <DragDropContext>
+        <div className="board">
+          <div className="columns-output-container">
+            {columns.map((col, index) => (
+              <Column
+                key={index}
+                column={col}
+                handleDeleteColumn={handleDeleteColumn}
+              />
+            ))}
+          </div>
+          <button
+            className="new-col-btn"
+            onClick={() => {
+              handleCreateColumn();
+            }}
+            aria-label="Add new column"
+          >
+            <span>New column</span>
+            <IoIosAddCircle />
+          </button>
+        </div>
+      </DragDropContext>
+    </>
+  );
 
-//   function handleCreateColumn() {
-//     setColumns((prevColumns) => {
-//       // Column to add
-//       const columnAdd = {
-//         id: prevColumns.length + 1,
-//         title: `Column ${prevColumns.length + 1}`,
-//       };
-//       // Return the updated columns array with the new column
-//       return [...prevColumns, columnAdd];
-//     });
-//   }
+  function handleCreateColumn() {
+    setColumns((prevColumns) => {
+      // Column to add
+      const columnAdd = {
+        id: prevColumns.length + 1,
+        title: `Column ${prevColumns.length + 1}`,
+      };
+      // Return the updated columns array with the new column
+      return [...prevColumns, columnAdd];
+    });
+  }
 
-//   function handleDeleteColumn(id) {
-//     const filteredColumns = columns.filter((col) => col.id !== id);
-//     setColumns(filteredColumns);
-//   }
-// };
+  function handleDeleteColumn(id) {
+    const filteredColumns = columns.filter((col) => col.id !== id);
+    setColumns(filteredColumns);
+  }
+};
 
-// export default Board;
+export default Board;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
-// import React from "react";
-// import { Draggable } from "react-beautiful-dnd";
+import React from "react";
+import { Draggable } from "react-beautiful-dnd";
 
-// const Card = ({ title, id, date }) => {
-//   return (
-//     <>
-//       <div>
-//         <h3>{Card.title}</h3>
-//       </div>
-//       <div>
-//         <p>{Card.id}</p>
-//         <p>{Card.date}</p>
-//       </div>
-//     </>
-//   );
-// };
+const Card = ({ title, id, date }) => {
+  return (
+    <>
+      <div>
+        <h3>{Card.title}</h3>
+      </div>
+      <div>
+        <p>{Card.id}</p>
+        <p>{Card.date}</p>
+      </div>
+    </>
+  );
+};
 
-// export default Card;
+export default Card;
