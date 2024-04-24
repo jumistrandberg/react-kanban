8696034bf5313813eadfae08d82c1dae7bf7ab67
Jumi <feliciastrandberg@gmail.com>
2024-04-24 12:48:46 +0200

    commented out bord return

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -55,38 +55,46 @@ const Board = () => {
     setIsModalOpen(false);
   };
 
-  return (
-    <>
-    <div className="bg-mainBackgroundColor text-mainTextColor">Tailwind?</div>
-      <div className="flex m-auto min-h-screen w-full items-center px-10">
-        <div className="grid grid-cols-3 gap-4">
-          {columns.map((col, index) => (
-            <Column
-              key={index}
-              column={col}
-              handleDeleteColumn={handleDeleteColumn}
-              handleCreateCard={handleCreateCard}
-              handleCardOpen={handleCardOpen}
-            />
-          ))}
-        </div>
-        <button
-          className="
-            flex items-center justify-center h-12 w-32 border border-gray-300 rounded-lg
-            hover:bg-gray-100 hover:border-gray-400 focus:outline-none
-          "
-          onClick={() => {
-            handleCreateColumn();
-          }}
-          aria-label="Add new column"
-        >
-          <span>New column</span>
-          <IoIosAddCircle className="ml-2" />
-        </button>
-      </div>
-      {isModalOpen && <Modal task={selectedCard} closeModal={closeModal} />}
-    </>
-  );
-};
+//   return (
+//     <>
+//       <div className="
+//       flex 
+//       m-auto 
+//       min-h-screen 
+//       w-full 
+//       items-center 
+//       px-[40]
+//       overflow-x-auto
+//       overflow-y-hidden"
+//       >
+//         <div className="grid grid-cols-3 gap-4">
+//           {columns.map((col, index) => (
+//             <Column
+//               key={index}
+//               column={col}
+//               handleDeleteColumn={handleDeleteColumn}
+//               handleCreateCard={handleCreateCard}
+//               handleCardOpen={handleCardOpen}
+//             />
+//           ))}
+//         </div>
+//         <button
+//           className="
+//             flex items-center justify-center h-12 w-32 border border-gray-300 rounded-lg
+//             hover:bg-gray-100 hover:border-gray-400 focus:outline-none
+//           "
+//           onClick={() => {
+//             handleCreateColumn();
+//           }}
+//           aria-label="Add new column"
+//         >
+//           <span>New column</span>
+//           <IoIosAddCircle className="ml-2" />
+//         </button>
+//       </div>
+//       {isModalOpen && <Modal task={selectedCard} closeModal={closeModal} />}
+//     </>
+//   );
+// };
 
 export default Board;
