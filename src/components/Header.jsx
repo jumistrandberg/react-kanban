f761a2600d729b751ad25616da870e74527c600a
Jumi <feliciastrandberg@gmail.com>
2024-05-14 11:57:38 +0200

    fixed some header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="text-mainBackgroundColor py-4 text-2xl font-bold flex align-center justify-center">KanBan Bajs</div>
   )
 }
 
